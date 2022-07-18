ab27c1643b7af0355a3b9ee67b56832b873feb38
rettichlp <rettichlp01@gmail.com>
2022-07-18 20:09:05 +0200

    Add new commandbase to existing commands

--- src/main/java/com/rettichlp/UnicacityAddon/commands/NearestATMCommand.java
+++ src/main/java/com/rettichlp/UnicacityAddon/commands/NearestATMCommand.java
@@ -1,25 +1,38 @@
 package com.rettichlp.UnicacityAddon.commands;
 
-import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
-import com.rettichlp.UnicacityAddon.base.command.UCCommand;
-import com.rettichlp.UnicacityAddon.base.command.UnicacityCommand;
+import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
 import com.rettichlp.UnicacityAddon.base.location.ATM;
 import com.rettichlp.UnicacityAddon.base.location.NavigationUtils;
 import com.rettichlp.UnicacityAddon.base.text.ColorCode;
 import com.rettichlp.UnicacityAddon.base.text.Message;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.event.ClickEvent;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class NearestATMCommand implements UnicacityCommand {
+public class NearestATMCommand extends CommandBase {
 
-    @Override
-    @UCCommand(value = "nearestatm", usage = "/%label%")
-    public boolean onCommand(UPlayer p, List<String> args) {
+    @Override public String getName() {
+        return "nearestatm";
+    }
+
+    @Override public String getUsage(ICommandSender sender) {
+        return "/nearestatm";
+    }
+
+    @Override public List<String> getAliases() {
+        return Arrays.asList("natm");
+    }
+
+    @Override public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         Map.Entry<Double, ATM> nearestATM = NavigationUtils.getNearestATM();
 
-        p.sendMessage(Message.getBuilder()
+        AbstractionLayer.getPlayer().sendMessage(Message.getBuilder()
                 .prefix()
                 .of("ATM").color(ColorCode.GRAY).advance()
                 .space()
@@ -33,7 +46,5 @@ public class NearestATMCommand implements UnicacityCommand {
                 .space()
                 .of("➡ Navi").color(ColorCode.RED).clickEvent(ClickEvent.Action.RUN_COMMAND, nearestATM.getValue().getNaviCommand()).advance()
                 .createComponent());
-
-        return true;
     }
 }
--- src/main/java/com/rettichlp/UnicacityAddon/commands/TriggerEventCommand.java
+++ src/main/java/com/rettichlp/UnicacityAddon/commands/TriggerEventCommand.java
@@ -1,36 +1,69 @@
 package com.rettichlp.UnicacityAddon.commands;
 
+import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
 import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
-import com.rettichlp.UnicacityAddon.base.command.UCCommand;
-import com.rettichlp.UnicacityAddon.base.command.UnicacityCommand;
+import com.rettichlp.UnicacityAddon.base.text.ColorCode;
+import com.rettichlp.UnicacityAddon.base.text.Message;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class TriggerEventCommand implements UnicacityCommand {
+public class TriggerEventCommand extends CommandBase {
 
-    @Override
-    @UCCommand(value = "triggerEvent", usage = "/%label%\n"
-            + "§aBomben-Timer:§7 bomb_placed, bomb_removed\n"
-            + "§aAuto-Info:§7 car_open, car_closed\n")
-    public boolean onCommand(UPlayer p, List<String> args) {
-        if (args.size() < 1) return false;
+    @Override public String getName() {
+        return "triggerevent";
+    }
 
-        switch (args.get(0)) {
-        case "bomb_placed":
-            p.sendMessageAsString("News: ACHTUNG! Es wurde eine Bombe in der Nähe von NAVIPUNKT gefunden!");
-            break;
-        case "bomb_removed":
-            p.sendMessageAsString("News: Die Bombe konnte erfolgreich entschärft werden!");
-            break;
-        case "car_open":
-            p.sendMessageAsString("[Car] Du hast deinen AUTONAME aufgeschlossen.");
-            break;
-        case "car_closed":
-            p.sendMessageAsString("[Car] Du hast deinen AUTONAME abgeschlossen.");
-            break;
-        default: return false;
+    @Override public String getUsage(ICommandSender sender) {
+        return "/triggerevent [TriggerableEvent]";
+    }
+
+    @Override public List<String> getAliases() {
+        return Arrays.asList("tevent");
+    }
+
+    @Override public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        UPlayer p = AbstractionLayer.getPlayer();
+
+        if (args.length == 1) {
+            TriggerableEvent triggerableEvent = TriggerableEvent.valueOf(args[0]);
+            p.sendMessageAsString(triggerableEvent.getTriggerMesage());
+        } else Message.getBuilder()
+                .error()
+                .space()
+                .of("Syntax: " + getUsage(sender)).color(ColorCode.GRAY).advance()
+                .sendTo(p.getPlayer());
+    }
+
+    @Override public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        if (args.length == 1) {
+            return getListOfStringsMatchingLastWord(args, Arrays.asList(TriggerableEvent.values()));
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    private enum TriggerableEvent {
+        BOMB_PLACED("News: ACHTUNG! Es wurde eine Bombe in der Nähe von NAVIPUNKT gefunden!"),
+        BOMB_REMOVED("News: Die Bombe konnte erfolgreich entschärft werden!"),
+        CAR_OPEN("[Car] Du hast deinen AUTONAME aufgeschlossen."),
+        CAR_CLOSED("[Car] Du hast deinen AUTONAME abgeschlossen.");
+
+        private final String triggerMesage;
+
+        TriggerableEvent(String triggerMesage) {
+            this.triggerMesage = triggerMesage;
         }
 
-        return true;
+        public String getTriggerMesage() {
+            return triggerMesage;
+        }
     }
 }
