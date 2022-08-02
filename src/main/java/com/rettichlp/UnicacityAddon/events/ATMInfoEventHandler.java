845a001cf941e7743deb18e4ed404e4d9851396f
rettichlp <rettichlp01@gmail.com>
2022-08-02 19:38:27 +0200

    Fix bug in ATMInfoEventHandler with ATM

--- src/main/java/com/rettichlp/UnicacityAddon/events/ATMInfoEventHandler.java
+++ src/main/java/com/rettichlp/UnicacityAddon/events/ATMInfoEventHandler.java
@@ -7,8 +7,6 @@ import com.rettichlp.UnicacityAddon.base.text.PatternHandler;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.regex.Matcher;
-
 /**
  * @author RettichLP
  */
@@ -16,11 +14,9 @@ public class ATMInfoEventHandler {
 
     @SubscribeEvent public boolean onClientChatReceive(ClientChatReceivedEvent e) {
         if (!ConfigElements.getEventATM()) return false;
-
         UPlayer p = AbstractionLayer.getPlayer();
 
-        Matcher kontoauszugMatcher = PatternHandler.KONTOAUSZUG_PATTERN.matcher(e.getMessage().getUnformattedText());
-        if (kontoauszugMatcher.find()) {
+        if (PatternHandler.KONTOAUSZUG_PATTERN.matcher(e.getMessage().getUnformattedText()).find()) {
 
             if (ConfigElements.getEventATMFBank()) {
                 p.sendChatMessage("/fbank");
