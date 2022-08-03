80fbf1767576472eee35194873584ca9832cc2f7
Dimiikou <leon.aissa@icloud.com>
2022-08-03 23:03:44 +0200

    Implements SmarktLocations

--- src/main/java/com/rettichlp/UnicacityAddon/UnicacityAddon.java
+++ src/main/java/com/rettichlp/UnicacityAddon/UnicacityAddon.java
@@ -13,6 +13,7 @@ import com.rettichlp.UnicacityAddon.commands.TriggerEventCommand;
 import com.rettichlp.UnicacityAddon.commands.faction.ASetBlacklistCommand;
 import com.rettichlp.UnicacityAddon.commands.faction.ReinforcementCommand;
 import com.rettichlp.UnicacityAddon.commands.faction.ShareLocationCommand;
+import com.rettichlp.UnicacityAddon.commands.faction.badfaction.SchmarzmarktLocationsCommand;
 import com.rettichlp.UnicacityAddon.commands.faction.polizei.ASUCommand;
 import com.rettichlp.UnicacityAddon.commands.faction.polizei.ModifyWantedsCommand;
 import com.rettichlp.UnicacityAddon.commands.faction.rettungsdienst.ARezeptAcceptCommand;
@@ -87,6 +88,7 @@ public class UnicacityAddon extends LabyModAddon {
         ClientCommandHandler.instance.registerCommand(new NearestATMCommand());
         ClientCommandHandler.instance.registerCommand(new NearestJobCommand());
         ClientCommandHandler.instance.registerCommand(new ReinforcementCommand());
+        ClientCommandHandler.instance.registerCommand(new SchmarzmarktLocationsCommand());
         ClientCommandHandler.instance.registerCommand(new ShareLocationCommand());
         ClientCommandHandler.instance.registerCommand(new TriggerEventCommand());
 
