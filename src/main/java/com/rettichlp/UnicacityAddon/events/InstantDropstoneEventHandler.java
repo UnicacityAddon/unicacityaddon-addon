b6f15a85ee2f664da29dd8a041e8af6453e2f18c
Dimiikou <leon.aissa@icloud.com>
2022-08-03 23:14:13 +0200

    Implements Instant Dropstone (not tested)

---
+++ src/main/java/com/rettichlp/UnicacityAddon/events/InstantDropstoneEventHandler.java
@@ -0,0 +1,24 @@
+package com.rettichlp.UnicacityAddon.events;
+
+import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
+import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * @author Dimiikou
+ */
+public class InstantDropstoneEventHandler {
+
+    @SubscribeEvent
+    public boolean onClientChatReceived(ClientChatReceivedEvent e) {
+        UPlayer p = AbstractionLayer.getPlayer();
+
+        if (!e.getMessage().getUnformattedText().equals("[Steinbruch] Bring alles nun zum Lagerraum.")) return false;
+
+        p.sendChatMessage("/dropstone");
+        p.stopRoute();
+
+        return false;
+    }
+}
