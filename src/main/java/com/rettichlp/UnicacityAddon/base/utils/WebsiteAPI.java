6893a1fdb20091059a05a4054437d3cfb60bd110
rettichlp <rettichlp01@gmail.com>
2022-08-10 18:57:29 +0200

    Fix crash on join if utility websites are down

--- src/main/java/com/rettichlp/UnicacityAddon/base/utils/WebsiteAPI.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/utils/WebsiteAPI.java
@@ -1,9 +1,11 @@
 package com.rettichlp.UnicacityAddon.base.utils;
 
+import joptsimple.internal.Strings;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Scanner;
 
 /**
@@ -12,14 +14,14 @@ import java.util.Scanner;
 public class WebsiteAPI {
 
     public static String websiteToString(String urlString) {
-
         Scanner scanner;
         StringBuilder websiteSource = new StringBuilder();
 
         if (urlString != null) {
             try {
-                URLConnection openConnection = new URL(urlString).openConnection();
+                HttpURLConnection openConnection = (HttpURLConnection) new URL(urlString).openConnection();
                 openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+                if (openConnection.getResponseCode() != HttpURLConnection.HTTP_OK) return Strings.EMPTY;
                 scanner = new Scanner(new InputStreamReader(openConnection.getInputStream()));
                 while (scanner.hasNextLine()) websiteSource.append(scanner.nextLine()).append("\n\r");
             } catch (IOException e) {
