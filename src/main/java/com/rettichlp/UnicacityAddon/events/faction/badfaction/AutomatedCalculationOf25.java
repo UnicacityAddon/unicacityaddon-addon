715413033499642b408d7d48afdde5e5c9b6b61b
Dimiikou <leon.aissa@icloud.com>
2022-08-14 23:44:52 +0200

    Adds dot to 25% calculation

--- src/main/java/com/rettichlp/UnicacityAddon/events/faction/badfaction/AutomatedCalculationOf25.java
+++ src/main/java/com/rettichlp/UnicacityAddon/events/faction/badfaction/AutomatedCalculationOf25.java
@@ -8,6 +8,8 @@ import com.rettichlp.UnicacityAddon.base.text.PatternHandler;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.regex.Matcher;
 
 /**
@@ -24,6 +26,7 @@ public class AutomatedCalculationOf25 {
             int cash = Integer.parseInt(statementOfAccountMatcher.group(2));
             int bankBalance = Integer.parseInt(statementOfAccountMatcher.group(3));
             int oneQuarterOfAll = (cash + bankBalance) / 4;
+            NumberFormat numberFormat = NumberFormat.getNumberInstance(new Locale("da", "DK"));
 
             Message.getBuilder()
                     .of("Finanzen von " + statementOfAccountMatcher.group(1)).color(ColorCode.GOLD).advance()
@@ -35,7 +38,7 @@ public class AutomatedCalculationOf25 {
                     .of(":").color(ColorCode.DARK_GRAY).advance().space()
                     .of(statementOfAccountMatcher.group(3) + "$").color(ColorCode.RED).advance().space()
                     .of("(").color(ColorCode.DARK_GRAY).advance()
-                    .of(oneQuarterOfAll + "$").color(ColorCode.RED).advance()
+                    .of(numberFormat.format(oneQuarterOfAll) + "$").color(ColorCode.RED).advance()
                     .of(")").color(ColorCode.DARK_GRAY).advance().sendTo(p.getPlayer());
 
             e.setCanceled(true);
