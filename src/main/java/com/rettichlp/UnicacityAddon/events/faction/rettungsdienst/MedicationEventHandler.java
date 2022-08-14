cb030cfc85c2a970c285428194ecefc128f4aa2e
rettichlp <rettichlp01@gmail.com>
2022-08-15 00:25:30 +0200

    Make acceptRecipe and giveRecipe public and static

--- src/main/java/com/rettichlp/UnicacityAddon/events/faction/rettungsdienst/MedicationEventHandler.java
+++ src/main/java/com/rettichlp/UnicacityAddon/events/faction/rettungsdienst/MedicationEventHandler.java
@@ -38,7 +38,7 @@ public class MedicationEventHandler {
         }, delay);
     }
 
-    private void acceptRecipe() {
+    public static void acceptRecipe() {
         --ARezeptAnnehmenCommand.amount;
         lastExecution = System.currentTimeMillis();
         AbstractionLayer.getPlayer().acceptOffer();
@@ -64,7 +64,7 @@ public class MedicationEventHandler {
         }, delay);
     }
 
-    private void giveRecipe() {
+    public static void giveRecipe() {
         --ARezeptCommand.amount;
         lastExecution = System.currentTimeMillis();
         AbstractionLayer.getPlayer().sellMedication(ARezeptCommand.target, ARezeptCommand.medication);
