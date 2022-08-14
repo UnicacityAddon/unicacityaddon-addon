07e19a1177be4dfd1897f7c6ebdf2c246413dc24
rettichlp <rettichlp01@gmail.com>
2022-08-14 21:13:19 +0200

    Update registries

--- src/main/java/com/rettichlp/UnicacityAddon/base/registry/EventRegistry.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/registry/EventRegistry.java
@@ -1,90 +1,12 @@
 package com.rettichlp.UnicacityAddon.base.registry;
 
-import com.rettichlp.UnicacityAddon.events.ABuyEventHandler;
-import com.rettichlp.UnicacityAddon.events.AEquipEventHandler;
-import com.rettichlp.UnicacityAddon.events.BombTimerEventHandler;
-import com.rettichlp.UnicacityAddon.events.CarEventHandler;
-import com.rettichlp.UnicacityAddon.events.FactionInfoEventHandler;
-import com.rettichlp.UnicacityAddon.events.HotkeyEventHandler;
-import com.rettichlp.UnicacityAddon.events.HouseRenterEventHandler;
-import com.rettichlp.UnicacityAddon.events.JoinEventHandler;
-import com.rettichlp.UnicacityAddon.events.KarmaMessageEventHandler;
-import com.rettichlp.UnicacityAddon.events.MobileEventHandler;
-import com.rettichlp.UnicacityAddon.events.MoneyEventHandler;
-import com.rettichlp.UnicacityAddon.events.NameTagEventHandler;
-import com.rettichlp.UnicacityAddon.events.PayDayEventHandler;
+import com.rettichlp.UnicacityAddon.UnicacityAddon;
+import com.rettichlp.UnicacityAddon.base.registry.annotation.UCEvent;
 import com.rettichlp.UnicacityAddon.events.TabListEventHandler;
-import com.rettichlp.UnicacityAddon.events.WeaponClickEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.AFbankEinzahlenEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.BlacklistEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.ContractEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.EmergencyServiceEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.FDoorEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.ReinforcementEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.ShareLocationEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.badfaction.AutomatedCalculationOf25EventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.badfaction.BlacklistInfoEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.badfaction.FBIHackEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.badfaction.GiftEigenbedarfEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.badfaction.ModifyBlacklistEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.badfaction.PlantTimerEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.polizei.HQMessageEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.polizei.WantedEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.rettungsdienst.MedicationEventHandler;
-import com.rettichlp.UnicacityAddon.events.faction.terroristen.ExplosiveBeltTimerEventHandler;
-import com.rettichlp.UnicacityAddon.events.job.ADropEventHandler;
-import com.rettichlp.UnicacityAddon.events.job.ADropMoneyEventHandler;
-import com.rettichlp.UnicacityAddon.events.job.FishermanEventHandler;
-import com.rettichlp.UnicacityAddon.events.job.InstantDropstoneEventHandler;
-import com.rettichlp.UnicacityAddon.events.team.ReportAcceptEventHandler;
-import net.labymod.api.LabyModAddon;
+import net.minecraftforge.fml.common.discovery.ASMDataTable;
 
 public class EventRegistry {
 
-    public static void register(LabyModAddon addon) {
-        // ForgeEvents -> https://docs.labymod.net/pages/create-addons/forge_events/
-        addon.getApi().registerForgeListener(new ABuyEventHandler());
-        addon.getApi().registerForgeListener(new AEquipEventHandler());
-        addon.getApi().registerForgeListener(new AFbankEinzahlenEventHandler());
-        addon.getApi().registerForgeListener(new AutomatedCalculationOf25EventHandler());
-        addon.getApi().registerForgeListener(new ADropEventHandler());
-        addon.getApi().registerForgeListener(new ADropMoneyEventHandler());
-        addon.getApi().registerForgeListener(new MoneyEventHandler());
-        addon.getApi().registerForgeListener(new BlacklistEventHandler());
-        addon.getApi().registerForgeListener(new BlacklistInfoEventHandler());
-        addon.getApi().registerForgeListener(new BombTimerEventHandler());
-        addon.getApi().registerForgeListener(new CarEventHandler());
-        addon.getApi().registerForgeListener(new ContractEventHandler());
-        addon.getApi().registerForgeListener(new EmergencyServiceEventHandler());
-        addon.getApi().registerForgeListener(new ExplosiveBeltTimerEventHandler());
-        addon.getApi().registerForgeListener(new FactionInfoEventHandler());
-        addon.getApi().registerForgeListener(new FBIHackEventHandler());
-        addon.getApi().registerForgeListener(new FDoorEventHandler());
-        addon.getApi().registerForgeListener(new FishermanEventHandler());
-        addon.getApi().registerForgeListener(new GiftEigenbedarfEventHandler());
-        addon.getApi().registerForgeListener(new HotkeyEventHandler());
-        addon.getApi().registerForgeListener(new HouseRenterEventHandler());
-        addon.getApi().registerForgeListener(new HQMessageEventHandler());
-        addon.getApi().registerForgeListener(new InstantDropstoneEventHandler());
-        addon.getApi().registerForgeListener(new JoinEventHandler());
-        addon.getApi().registerForgeListener(new KarmaMessageEventHandler());
-        addon.getApi().registerForgeListener(new MedicationEventHandler());
-        addon.getApi().registerForgeListener(new MobileEventHandler());
-        addon.getApi().registerForgeListener(new ModifyBlacklistEventHandler());
-        addon.getApi().registerForgeListener(new NameTagEventHandler());
-        addon.getApi().registerForgeListener(new PayDayEventHandler());
-        addon.getApi().registerForgeListener(new PlantTimerEventHandler());
-        addon.getApi().registerForgeListener(new ReinforcementEventHandler());
-        addon.getApi().registerForgeListener(new ReportAcceptEventHandler());
-        addon.getApi().registerForgeListener(new ShareLocationEventHandler());
-        addon.getApi().registerForgeListener(new WantedEventHandler());
-        addon.getApi().registerForgeListener(new WeaponClickEventHandler());
-
-        // LabyModEvents -> https://docs.labymod.net/pages/create-addons/labymod_events/
-        addon.getApi().getEventManager().register(new TabListEventHandler());
-    }
-
-    /*
     public static void register(ASMDataTable asmDataTable) {
         // ForgeEvents -> https://docs.labymod.net/pages/create-addons/forge_events/
         asmDataTable.getAll(UCEvent.class.getCanonicalName()).forEach(asmData -> {
@@ -92,20 +14,13 @@ public class EventRegistry {
                 Class<?> clazz = Class.forName(asmData.getClassName());
                 UnicacityAddon.ADDON.getApi().registerForgeListener(clazz.newInstance());
                 System.out.println("UCEvent: " + clazz.getSimpleName());
-            } catch (ClassNotFoundException e) {
-                // TODO: 14.08.2022
-                throw new RuntimeException(e);
-            } catch (InstantiationException e) {
-                // TODO: 14.08.2022
-                throw new RuntimeException(e);
-            } catch (IllegalAccessException e) {
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
                 // TODO: 14.08.2022
                 throw new RuntimeException(e);
             }
         });
-    }
 
-    // LabyModEvents -> https://docs.labymod.net/pages/create-addons/labymod_events/
-    UnicacityAddon.ADDON.getApi().getEventManager().register(new TabListEventHandler());
-    */
+        // LabyModEvents -> https://docs.labymod.net/pages/create-addons/labymod_events/
+        UnicacityAddon.ADDON.getApi().getEventManager().register(new TabListEventHandler());
+    }
 }
--- src/main/java/com/rettichlp/UnicacityAddon/base/registry/ModuleRegistry.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/registry/ModuleRegistry.java
@@ -1,41 +1,17 @@
 package com.rettichlp.UnicacityAddon.base.registry;
 
-import com.rettichlp.UnicacityAddon.modules.BankMoneyModule;
-import com.rettichlp.UnicacityAddon.modules.BombTimerModule;
-import com.rettichlp.UnicacityAddon.modules.CarOpenModule;
-import com.rettichlp.UnicacityAddon.modules.CashMoneyModule;
-import com.rettichlp.UnicacityAddon.modules.EmergencyServiceModule;
-import com.rettichlp.UnicacityAddon.modules.ExplosiveBeltTimerModule;
-import com.rettichlp.UnicacityAddon.modules.FBIHackModule;
-import com.rettichlp.UnicacityAddon.modules.JobMoneyModule;
-import com.rettichlp.UnicacityAddon.modules.PayDayModule;
-import com.rettichlp.UnicacityAddon.modules.PlantFertilizeTimerModule;
-import com.rettichlp.UnicacityAddon.modules.PlantWaterTimerModule;
-import net.labymod.api.LabyModAddon;
+import com.rettichlp.UnicacityAddon.UnicacityAddon;
+import com.rettichlp.UnicacityAddon.base.registry.annotation.UCModule;
+import net.labymod.ingamegui.Module;
 import net.labymod.ingamegui.ModuleCategory;
 import net.labymod.settings.elements.ControlElement;
 import net.labymod.utils.Material;
+import net.minecraftforge.fml.common.discovery.ASMDataTable;
 
 public class ModuleRegistry {
 
     public static final ModuleCategory UNICACITY = new ModuleCategory("Unicacity", true, new ControlElement.IconData(Material.DIAMOND));
 
-    public static void register(LabyModAddon addon) {
-        // Modules -> https://docs.labymod.net/pages/create-addons/module_system/
-        addon.getApi().registerModule(new BankMoneyModule());
-        addon.getApi().registerModule(new BombTimerModule());
-        addon.getApi().registerModule(new CarOpenModule());
-        addon.getApi().registerModule(new CashMoneyModule());
-        addon.getApi().registerModule(new EmergencyServiceModule());
-        addon.getApi().registerModule(new ExplosiveBeltTimerModule());
-        addon.getApi().registerModule(new FBIHackModule());
-        addon.getApi().registerModule(new JobMoneyModule());
-        addon.getApi().registerModule(new PayDayModule());
-        addon.getApi().registerModule(new PlantFertilizeTimerModule());
-        addon.getApi().registerModule(new PlantWaterTimerModule());
-    }
-
-    /*
     public static void register(ASMDataTable asmDataTable) {
         // Modules -> https://docs.labymod.net/pages/create-addons/module_system/
         asmDataTable.getAll(UCModule.class.getCanonicalName()).forEach(asmData -> {
@@ -43,17 +19,10 @@ public class ModuleRegistry {
                 Class<?> clazz = Class.forName(asmData.getClassName());
                 UnicacityAddon.ADDON.getApi().registerModule((Module) clazz.newInstance());
                 System.out.println("UCModule: " + clazz.getSimpleName());
-            } catch (ClassNotFoundException e) {
-                // TODO: 14.08.2022
-                throw new RuntimeException(e);
-            } catch (InstantiationException e) {
-                // TODO: 14.08.2022
-                throw new RuntimeException(e);
-            } catch (IllegalAccessException e) {
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
                 // TODO: 14.08.2022
                 throw new RuntimeException(e);
             }
         });
     }
-    */
 }
