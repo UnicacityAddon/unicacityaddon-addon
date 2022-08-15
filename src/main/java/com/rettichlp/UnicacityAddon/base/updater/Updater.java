9dc0277ad6ab9d31e6021da90cf4d954f5337f0c
rettichlp <rettichlp01@gmail.com>
2022-08-15 15:09:11 +0200

    Add chat message to Updater.updateChecker()

--- src/main/java/com/rettichlp/UnicacityAddon/base/updater/Updater.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/updater/Updater.java
@@ -1,7 +1,11 @@
 package com.rettichlp.UnicacityAddon.base.updater;
 
 import com.rettichlp.UnicacityAddon.UnicacityAddon;
+import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
 import com.rettichlp.UnicacityAddon.base.text.ColorCode;
+import com.rettichlp.UnicacityAddon.base.text.Message;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,11 +14,32 @@ import java.net.URLConnection;
 
 public class Updater {
 
-    public static void update() {
+    public static void updateChecker() {
         String latestVersion = getLatestVersion();
         if (latestVersion.equals(UnicacityAddon.VERSION)) return;
 
-        UnicacityAddon.LABYMOD.notifyMessageRaw(ColorCode.RED.getCode() + "Update verfügbar!", "Es ist Version " + ColorCode.GOLD.getCode() + latestVersion + ColorCode.WHITE.getCode() + " verfügbar.");
+        UnicacityAddon.LABYMOD.notifyMessageRaw(ColorCode.AQUA.getCode() + "Update verfügbar!", "Es ist Version " + ColorCode.DARK_AQUA.getCode() + latestVersion + ColorCode.WHITE.getCode() + " verfügbar.");
+
+        AbstractionLayer.getPlayer().sendMessage(Message.getBuilder()
+                .info().space()
+                .of("Es ist ein neues Update für").advance().space()
+                .of("UnicacityAddon")
+                        .color(ColorCode.DARK_AQUA)
+                        .underline()
+                        .hoverEvent(HoverEvent.Action.SHOW_TEXT, Message.getBuilder()
+                                .of("Changelog").color(ColorCode.RED).advance()
+                                .createComponent())
+                        .clickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/rettichlp/UnicacityAddon-1.12.2/releases/latest")
+                        .advance().space()
+                .of("verfügbar!").advance().space()
+                .of("Aktualisieren")
+                        .color(ColorCode.GOLD)
+                        .hoverEvent(HoverEvent.Action.SHOW_TEXT, Message.getBuilder()
+                                .of("Aktualisieren").color(ColorCode.RED).advance()
+                                .createComponent())
+                        .clickEvent(ClickEvent.Action.RUN_COMMAND, "/updateunicacityaddon")
+                        .advance()
+                .createComponent());
     }
 
     private static String getLatestVersion() {
