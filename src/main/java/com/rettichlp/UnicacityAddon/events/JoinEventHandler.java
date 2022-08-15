5dbeb03002d1071af6aeb28f7267f0aeeff5576a
rettichlp <rettichlp01@gmail.com>
2022-08-15 14:54:59 +0200

    Move login depending code from MobileEventHandler to JoinEventHandler, rewrite JoinEventHandler

--- src/main/java/com/rettichlp/UnicacityAddon/events/JoinEventHandler.java
+++ src/main/java/com/rettichlp/UnicacityAddon/events/JoinEventHandler.java
@@ -1,9 +1,11 @@
 package com.rettichlp.UnicacityAddon.events;
 
+import com.rettichlp.UnicacityAddon.UnicacityAddon;
 import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
 import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
 import com.rettichlp.UnicacityAddon.base.config.ConfigElements;
 import com.rettichlp.UnicacityAddon.base.text.PatternHandler;
+import com.rettichlp.UnicacityAddon.base.updater.Updater;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -19,68 +21,71 @@ public class JoinEventHandler {
 
     @SubscribeEvent
     public boolean onClientChatReceived(ClientChatReceivedEvent e) {
-        if (e.getMessage().getUnformattedText().equals("  Info: Gebe bitte dein Passwort ein. /passwort [Passwort]")) {
-            accountLocked = true;
-            if (ConfigElements.getPasswordAutomation() && (ConfigElements.getPassword() != null))
-                AbstractionLayer.getPlayer().sendChatMessage("/passwort " + ConfigElements.getPassword());
+        String msg = e.getMessage().getUnformattedText();
+        if (!UnicacityAddon.isUnicacity()) return false;
 
+        if (PatternHandler.ACCOUNT_WELCOME_BACK_PATTERN.matcher(msg).find()) {
+            MobileEventHandler.activeCommunicationsCheck = true;
+            handleJoin();
             return false;
         }
 
-        UPlayer p = AbstractionLayer.getPlayer();
-
-        if (PatternHandler.ACCOUNT_UNLOCKED_PATTERN.matcher(e.getMessage().getUnformattedText()).find()) {
-            accountLocked = false;
-            if (!ConfigElements.getCommandAutomation()) return false;
-            Timer t = new Timer();
-
-            if (!ConfigElements.getFirstCommand().isEmpty()) p.sendChatMessage(ConfigElements.getFirstCommand());
-
-            t.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    if (!ConfigElements.getSecondCommand().isEmpty()) p.sendChatMessage(ConfigElements.getSecondCommand());
-                }
-            }, 1000L);
-
-            t.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    if (!ConfigElements.getThirdCommand().isEmpty()) p.sendChatMessage(ConfigElements.getThirdCommand());
-                }
-            }, 2000L);
-
+        if (e.getMessage().getUnformattedText().equals("  Info: Gebe bitte dein Passwort ein. /passwort [Passwort]")) { // TODO: 15.08.2022 Pattern
+            accountLocked = true;
+            handleUnlockAccount();
         }
 
-        if (PatternHandler.ACCOUNT_WELCOME_BACK_PATTERN.matcher(e.getMessage().getUnformattedText()).find()) {
-            Timer t = new Timer();
+        if (PatternHandler.ACCOUNT_UNLOCKED_PATTERN.matcher(msg).find()) {
+            accountLocked = false;
+            handleJoin();
+        }
 
-            TimerTask tt = new TimerTask() {
-                @Override
-                public void run() {
-                    if (!accountLocked && ConfigElements.getCommandAutomation()) {
-                        if (!ConfigElements.getFirstCommand().isEmpty())
-                            p.sendChatMessage(ConfigElements.getFirstCommand());
+        return false;
+    }
 
-                        t.schedule(new TimerTask() {
-                            @Override
-                            public void run() {
-                                if (!ConfigElements.getSecondCommand().isEmpty())
-                                    p.sendChatMessage(ConfigElements.getSecondCommand());
-                            }
-                        }, 1000L);
+    private void handleUnlockAccount() {
+        if (!accountLocked) return;
+        if (ConfigElements.getPasswordAutomation() && (ConfigElements.getPassword() != null))
+            AbstractionLayer.getPlayer().sendChatMessage("/passwort " + ConfigElements.getPassword());
+    }
 
-                        t.schedule(new TimerTask() {
-                            @Override
-                            public void run() {
-                                if (!ConfigElements.getThirdCommand().isEmpty())
-                                    p.sendChatMessage(ConfigElements.getThirdCommand());
-                            }
-                        }, 2000L);
+    private void handleJoin() {
+        UPlayer p = AbstractionLayer.getPlayer();
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (accountLocked) return;
+
+                // MOBILEEVENTHANDLER
+                p.sendChatMessage("/mobile");
+
+                // AUTOMATE_COMMAND_SETTINGS
+                if (!ConfigElements.getCommandAutomation()) return;
+
+                // AUTOMATE_COMMAND_FIRST_SETTINGS
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (!ConfigElements.getSecondCommand().isEmpty()) p.sendChatMessage(ConfigElements.getFirstCommand());
                     }
-                }
-            };
-            t.schedule(tt, 200);
+                }, 500);
+
+                // AUTOMATE_COMMAND_SECOND_SETTINGS
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (!ConfigElements.getSecondCommand().isEmpty()) p.sendChatMessage(ConfigElements.getSecondCommand());
+                    }
+                }, 1000);
+
+                // AUTOMATE_COMMAND_THIRD_SETTINGS
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (!ConfigElements.getThirdCommand().isEmpty()) p.sendChatMessage(ConfigElements.getThirdCommand());
+                    }
+                }, 1500);
 
         }
 
