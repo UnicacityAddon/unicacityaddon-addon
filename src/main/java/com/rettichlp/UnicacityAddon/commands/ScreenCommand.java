a7def66871c66b8df71257285febcd84a82c7f98
rettichlp <rettichlp01@gmail.com>
2022-08-16 10:47:48 +0200

    Update ScreenCommand, remove handleScreenshot, call handleScreenshot from HotkeyEventHandler

--- src/main/java/com/rettichlp/UnicacityAddon/commands/ScreenCommand.java
+++ src/main/java/com/rettichlp/UnicacityAddon/commands/ScreenCommand.java
@@ -1,32 +1,28 @@
 package com.rettichlp.UnicacityAddon.commands;
 
-import com.rettichlp.UnicacityAddon.UnicacityAddon;
 import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
+import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
 import com.rettichlp.UnicacityAddon.base.io.FileManager;
-import com.rettichlp.UnicacityAddon.base.reflection.ReflectionUtils;
-import com.rettichlp.UnicacityAddon.base.text.ColorCode;
-import net.minecraft.client.shader.Framebuffer;
+import com.rettichlp.UnicacityAddon.events.HotkeyEventHandler;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ScreenShotHelper;
 import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ScreenCommand extends CommandBase {
 
-    private static List<String> variables = Arrays.asList(
+    private final List<String> SCREENSHOT_TYPE_LIST = new ArrayList<>(Arrays.asList(
             "kills", "großeinsatz", "drogeneinnahme", "equip", "reinforcement",
-            "roleplay", "verhaftung", "korruption", "ticket", "blacklist");
+            "roleplay", "verhaftung", "korruption", "ticket", "blacklist"));
 
     @Override
     @Nonnull
@@ -53,36 +49,33 @@ public class ScreenCommand extends CommandBase {
 
     @Override
     public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) {
-        if (args.length != 1 || !variables.contains(args[0].toLowerCase())) { //  || !variables.contains(args[0].toLowerCase())
-            AbstractionLayer.getPlayer().sendSyntaxMessage(getUsage(sender));
+        UPlayer p = AbstractionLayer.getPlayer();
+
+        if (args.length < 1) {
+            p.sendSyntaxMessage(getUsage(sender));
             return;
         }
 
-        handleScreenshot(args[0]);
+        String name = SCREENSHOT_TYPE_LIST.stream().filter(s -> s.contains(args[0])).findFirst().orElse(null);
+        if (name == null) {
+            p.sendSyntaxMessage(getUsage(sender));
+            return;
+        }
+
+        try {
+            File file = FileManager.getNewActivityImageFile(args[0]);
+            HotkeyEventHandler.handleScreenshot(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     @Nonnull
     public List<String> getTabCompletions(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
-        return variables;
-    }
-
-    private void handleScreenshot(String type) {
-        try {
-            File newImageFile = FileManager.getNewActivityImageFile(type);
-            if (newImageFile == null) {
-                UnicacityAddon.LABYMOD.notifyMessageRaw(ColorCode.RED.getCode() + "Fehler!", "Screenshot konnte nicht erstellt werden.");
-                return;
-            }
-
-            Framebuffer framebuffer = ReflectionUtils.getValue(UnicacityAddon.MINECRAFT, Framebuffer.class);
-            assert framebuffer != null;
-            BufferedImage image = ScreenShotHelper.createScreenshot(UnicacityAddon.MINECRAFT.displayWidth, UnicacityAddon.MINECRAFT.displayHeight, framebuffer);
-            ImageIO.write(image, "jpg", newImageFile);
-            UnicacityAddon.LABYMOD.notifyMessageRaw(ColorCode.GREEN.getCode() + "Screenshot erstellt!", "");
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        List<String> tabCompletions = SCREENSHOT_TYPE_LIST;
+        String input = args[args.length - 1].toLowerCase().replace('-', ' ');
+        tabCompletions.removeIf(tabComplete -> !tabComplete.toLowerCase().startsWith(input));
+        return tabCompletions;
     }
 }
