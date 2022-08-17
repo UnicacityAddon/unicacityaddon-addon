739f4a92c0397fe5a7033f3f97a0b30fb44755bd
rettichlp <rettichlp01@gmail.com>
2022-08-17 12:55:38 +0200

    Add public channel id for ts to Faction

--- src/main/java/com/rettichlp/UnicacityAddon/base/faction/Faction.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/faction/Faction.java
@@ -13,56 +13,56 @@ import java.util.stream.Collectors;
  */
 public enum Faction {
 
-    NULL("", "Keine Auswahl", "", ""),
-    CALDERON("calderon", "Calderon Kartell", "Kartell", Message.getBuilder()
+    NULL("", "Keine Auswahl", "", -1, ""),
+    CALDERON("calderon", "Calderon Kartell", "Kartell", 154, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("☀").color(ColorCode.GOLD).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    FBI("fbi", "F.B.I", "FBI", Message.getBuilder()
+    FBI("fbi", "F.B.I", "FBI", 106, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✯").color(ColorCode.DARK_BLUE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    HITMAN("hitman", "Hitman", "Hitman", Message.getBuilder()
+    HITMAN("hitman", "Hitman", "Hitman", 215, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("➹").color(ColorCode.AQUA).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    KERZAKOV("kerzakov", "Kerzakov Familie", "Kerzakov", Message.getBuilder()
+    KERZAKOV("kerzakov", "Kerzakov Familie", "Kerzakov", 166, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✮").color(ColorCode.RED).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    KIRCHE("kirche", "Kirche", "Kirche", Message.getBuilder()
+    KIRCHE("kirche", "Kirche", "Kirche", 227, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("†").color(ColorCode.LIGHT_PURPLE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    LACOSANOSTRA("lacosanostra", "La Cosa Nostra", "Mafia", Message.getBuilder()
+    LACOSANOSTRA("lacosanostra", "La Cosa Nostra", "Mafia", 130, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("⚜").color(ColorCode.DARK_AQUA).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    NEWS("news", "News", "News", Message.getBuilder()
+    NEWS("news", "News", "News", 239, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✉").color(ColorCode.YELLOW).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    OBRIEN("obrien", "O'brien", "Obrien", Message.getBuilder()
+    OBRIEN("obrien", "O'brien", "Obrien", 191, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("☘").color(ColorCode.DARK_GREEN).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    POLIZEI("polizei", "Polizei", "Polizei", Message.getBuilder()
+    POLIZEI("polizei", "Polizei", "Polizei", 78, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✯").color(ColorCode.BLUE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    RETTUNGSDIENST("rettungsdienst", "Rettungsdienst", "Rettungsdienst", Message.getBuilder()
+    RETTUNGSDIENST("rettungsdienst", "Rettungsdienst", "Rettungsdienst", 118, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✚").color(ColorCode.DARK_RED).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    TERRORISTEN("terroristen", "Terroristen", "Terroristen", Message.getBuilder()
+    TERRORISTEN("terroristen", "Terroristen", "Terroristen", 203, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("❇").color(ColorCode.GRAY).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    TRIADEN("triaden", "Triaden", "Triaden", Message.getBuilder()
+    TRIADEN("triaden", "Triaden", "Triaden", 179, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("是").color(ColorCode.RED).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    WESTSIDEBALLAS("westsideballas", "Westside Ballas", "Gang", Message.getBuilder()
+    WESTSIDEBALLAS("westsideballas", "Westside Ballas", "Gang", 142, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("☠").color(ColorCode.DARK_PURPLE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create());
@@ -70,12 +70,14 @@ public enum Faction {
     private final String apiName;
     private final String displayName;
     private final String factionKey;
+    private final int publicChannelId;
     private final String nameTagSuffix;
 
-    Faction(String apiName, String displayName, String factionKey, String nameTagSuffix) {
+    Faction(String apiName, String displayName, String factionKey, int publicChannelId, String nameTagSuffix) {
         this.apiName = apiName;
         this.displayName = displayName;
         this.factionKey = factionKey;
+        this.publicChannelId = publicChannelId;
         this.nameTagSuffix = nameTagSuffix;
     }
 
@@ -116,5 +118,9 @@ public enum Faction {
             if (faction.getFactionKey().equalsIgnoreCase(s)) return faction;
         }
         return null;
+    }
+
+    public int getPublicChannelId() {
+        return publicChannelId;
     }
 }
