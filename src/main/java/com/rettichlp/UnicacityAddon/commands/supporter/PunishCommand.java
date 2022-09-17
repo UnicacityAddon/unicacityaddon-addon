697ce3ad4039e78ec98e00c5e47587a039341a10
Dimiikou <leon.aissa@icloud.com>
2022-09-17 23:14:21 +0200

    Bugfix and code Cleanup

--- src/main/java/com/rettichlp/UnicacityAddon/commands/supporter/PunishCommand.java
+++ src/main/java/com/rettichlp/UnicacityAddon/commands/supporter/PunishCommand.java
@@ -64,12 +64,16 @@ public class PunishCommand extends CommandBase {
         String reason = punishment.getReason();
         int banDuration = punishment.getBanDuration();
 
-        if (punishment.getCheckpoints() > 0) p.sendChatMessage("/checkpoints " + args[0] + " " + punishment.getCheckpoints() + " " + reason);
-        if (punishment.getBanDuration() > 0) p.sendChatMessage("/tban " + args[0] + " " + getBanDurationString(banDuration) + " " + reason);
+        if (punishment.getCheckpoints() > 0)
+            p.sendChatMessage("/checkpoints " + args[0] + " " + punishment.getCheckpoints() + " " + reason);
+        if (punishment.getBanDuration() > 0)
+            p.sendChatMessage("/tban " + args[0] + " " + getBanDurationString(banDuration) + " " + reason);
         if (punishment.getBanDuration() == -1) p.sendChatMessage("/ban " + args[0] + " " + reason);
         if (punishment.isLoyalityPointReset()) p.sendChatMessage("/resettreuebonus " + args[0]);
-        if (punishment.getWeaponLock() > 0) p.sendChatMessage("/waffensperre " + args[0] + " " + punishment.getWeaponLock() + " 0 0");
-        if (punishment.getFactionLock() > 0) p.sendChatMessage("/fraksperre " + args[0] + " " + punishment.getFactionLock() + " " + reason);
+        if (punishment.getWeaponLock() > 0)
+            p.sendChatMessage("/waffensperre " + args[0] + " " + punishment.getWeaponLock() + " 0 0");
+        if (punishment.getFactionLock() > 0)
+            p.sendChatMessage("/fraksperre " + args[0] + " " + punishment.getFactionLock() + " " + reason);
         if (punishment.isKick()) p.sendChatMessage("/kick " + args[0] + " " + reason);
         if (punishment.getWarnAmmount() > 0)
             for (int i = 0; i < punishment.getWarnAmmount(); i++) {
@@ -106,7 +110,7 @@ public class PunishCommand extends CommandBase {
 
     private Punishment getPunishmentByName(String s) {
         for (Punishment punishment : Punishment.values()) {
-            if (punishment.getReason().equals(s)) return punishment;
+            if (punishment.getTabReason().equals(s)) return punishment;
         }
         return null;
     }
