466d7af0cfcf5851ba5ea0ee9ae67f2f5cc67247
rettichlp <rettichlp01@gmail.com>
2022-10-08 18:39:29 +0200

    Make statistic add requests asynchron

--- src/main/java/com/rettichlp/UnicacityAddon/base/api/request/APIRequest.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/api/request/APIRequest.java
@@ -247,41 +247,49 @@ public class APIRequest {
     public static void sendStatisticAddKillRequest() {
         Map<String, String> parameters = new HashMap<>();
 
-        try {
-            APIResponseHandler.getStatisticAddKillResponse(parameters);
-        } catch (APIUnsuccessResponseException e) {
-            e.sendInfoMessage();
-        }
+        new Thread(() -> {
+            try {
+                APIResponseHandler.getStatisticAddKillResponse(parameters);
+            } catch (APIUnsuccessResponseException e) {
+                e.sendInfoMessage();
+            }
+        }).start();
     }
 
     public static void sendStatisticAddDeathRequest() {
         Map<String, String> parameters = new HashMap<>();
 
-        try {
-            APIResponseHandler.getStatisticAddDeathResponse(parameters);
-        } catch (APIUnsuccessResponseException e) {
-            e.sendInfoMessage();
-        }
+        new Thread(() -> {
+            try {
+                APIResponseHandler.getStatisticAddDeathResponse(parameters);
+            } catch (APIUnsuccessResponseException e) {
+                e.sendInfoMessage();
+            }
+        }).start();
     }
 
     public static void sendStatisticAddReviveRequest() {
         Map<String, String> parameters = new HashMap<>();
 
-        try {
-            APIResponseHandler.getStatisticAddReviveResponse(parameters);
-        } catch (APIUnsuccessResponseException e) {
-            e.sendInfoMessage();
-        }
+        new Thread(() -> {
+            try {
+                APIResponseHandler.getStatisticAddReviveResponse(parameters);
+            } catch (APIUnsuccessResponseException e) {
+                e.sendInfoMessage();
+            }
+        }).start();
     }
 
     public static void sendStatisticAddServiceRequest() {
         Map<String, String> parameters = new HashMap<>();
 
-        try {
-            APIResponseHandler.getStatisticAddServiceResponse(parameters);
-        } catch (APIUnsuccessResponseException e) {
-            e.sendInfoMessage();
-        }
+        new Thread(() -> {
+            try {
+                APIResponseHandler.getStatisticAddServiceResponse(parameters);
+            } catch (APIUnsuccessResponseException e) {
+                e.sendInfoMessage();
+            }
+        }).start();
     }
 
     public static JsonObject sendTokenCreateRequest() {
