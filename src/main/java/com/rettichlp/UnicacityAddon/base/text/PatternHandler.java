8bc459eeaca05d1d64837020f05f4ca7fce68a04
rettichlp <rettichlp01@gmail.com>
2022-10-11 09:47:57 +0200

    Fix pattern for sub faction equip

--- src/main/java/com/rettichlp/UnicacityAddon/base/text/PatternHandler.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/text/PatternHandler.java
@@ -297,7 +297,7 @@ public class PatternHandler {
     /**
      * {@link com.rettichlp.UnicacityAddon.events.faction.EquipEventHandler}
      */
-    public static final Pattern EQUIP_PATTERN = Pattern.compile("^\\[Equip] Du hast dir (|ein |eine |einen |einem )([a-zA-Z-äöüßÄÖÜ ]+) equippt!$");
+    public static final Pattern EQUIP_PATTERN = Pattern.compile("^(|\\[Equip] )Du hast (dir|dich mit) (|ein |eine |einen |einem )([a-zA-Z-äöüßÄÖÜ ]+) equip(|p)t[!.]$");
     public static final Pattern TRACKER_PATTERN = Pattern.compile("^Du hast einen Peilsender an (?:\\[UC])*(\\w+) befestigt\\.$");
 
     /**
