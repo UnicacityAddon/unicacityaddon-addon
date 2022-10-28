078897c030608f47eef86024484d729aca38a3b5
Dimiikou <leon.aissa@icloud.com>
2022-10-28 09:09:46 +0200

    Append time to Bomb End Message

--- src/main/java/com/rettichlp/UnicacityAddon/events/faction/terroristen/BombTimerEventHandler.java
+++ src/main/java/com/rettichlp/UnicacityAddon/events/faction/terroristen/BombTimerEventHandler.java
@@ -1,10 +1,14 @@
 package com.rettichlp.UnicacityAddon.events.faction.terroristen;
 
 import com.rettichlp.UnicacityAddon.base.registry.annotation.UCEvent;
+import com.rettichlp.UnicacityAddon.base.text.ColorCode;
+import com.rettichlp.UnicacityAddon.base.text.Message;
 import com.rettichlp.UnicacityAddon.base.text.PatternHandler;
 import com.rettichlp.UnicacityAddon.modules.BombTimerModule;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.regex.Matcher;
 
 /**
  * @author RettichLP
@@ -22,7 +26,13 @@ public class BombTimerEventHandler {
             return false;
         }
 
-        if (PatternHandler.BOMB_REMOVED_PATTERN.matcher(msg).find()) BombTimerModule.stopBombTimer();
+        Matcher m = PatternHandler.BOMB_REMOVED_PATTERN.matcher(msg);
+        if (m.find()) {
+            BombTimerModule.stopBombTimer();
+            e.setMessage(Message.getBuilder().of("News: Die Bombe konnte").color(ColorCode.GOLD).bold().advance()
+                    .of(m.group(1)).color(ColorCode.GOLD).bold().advance()
+                    .of("entschärft werden! (" + BombTimerModule.timer + ")").color(ColorCode.GOLD).bold().advance().createComponent());
+        }
         return false;
     }
 }
