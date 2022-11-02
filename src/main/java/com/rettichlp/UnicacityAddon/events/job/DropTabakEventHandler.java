3e7388008702ba6ce424cdb712e762c66677a213
rettichlp <rettichlp01@gmail.com>
2022-11-02 03:09:53 +0100

    Add DropTabakEventHandler and pattern in PatternHandler

---
+++ src/main/java/com/rettichlp/UnicacityAddon/events/job/DropTabakEventHandler.java
@@ -0,0 +1,36 @@
+package com.rettichlp.UnicacityAddon.events.job;
+
+import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
+import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
+import com.rettichlp.UnicacityAddon.base.registry.annotation.UCEvent;
+import com.rettichlp.UnicacityAddon.base.text.PatternHandler;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * @author RettichLP
+ */
+@UCEvent
+public class DropTabakEventHandler {
+
+    private static boolean isTabakJob = false;
+
+    @SubscribeEvent
+    public void onClientChatReceive(ClientChatReceivedEvent e) {
+        String msg = e.getMessage().getUnformattedText();
+        UPlayer p = AbstractionLayer.getPlayer();
+
+        if (PatternHandler.TABAK_DROP_PATTERN.matcher(msg).find()) {
+            isTabakJob = true;
+            return;
+        }
+
+        if (PatternHandler.TABAK_FINISH_PATTERN.matcher(msg).find()) {
+            isTabakJob = false;
+            return;
+        }
+
+        if (msg.equals("Du hast dein Ziel erreicht.") && isTabakJob)
+            p.sendChatMessage("/droptabak");
+    }
+}
