0247a064c50f0c8bac2ff2b0de8ab5687dd3b36f
rettichlp <rettichlp01@gmail.com>
2022-11-20 03:58:26 +0100

    Update default tab completion

--- src/main/java/com/rettichlp/UnicacityAddon/commands/ReplyCommand.java
+++ src/main/java/com/rettichlp/UnicacityAddon/commands/ReplyCommand.java
@@ -43,7 +43,10 @@ public class ReplyCommand implements IClientCommand {
     @Override
     @Nonnull
     public List<String> getTabCompletions(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args, @Nullable BlockPos targetPos) {
-        return ForgeUtils.getOnlinePlayers();
+        List<String> tabCompletions = ForgeUtils.getOnlinePlayers();
+        String input = args[args.length - 1].toLowerCase();
+        tabCompletions.removeIf(tabComplete -> !tabComplete.toLowerCase().startsWith(input));
+        return tabCompletions;
     }
 
     @Override
