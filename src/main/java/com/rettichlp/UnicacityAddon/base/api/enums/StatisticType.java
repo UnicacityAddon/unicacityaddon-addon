1b93ac1d4e1c874029196b6f4cb76c8dca39abff
rettichlp <rettichlp01@gmail.com>
2022-11-21 12:37:30 +0100

    Add StatisticType as enum and call them in statistic requests

---
+++ src/main/java/com/rettichlp/UnicacityAddon/base/api/enums/StatisticType.java
@@ -0,0 +1,9 @@
+package com.rettichlp.UnicacityAddon.base.api.enums;
+
+public enum StatisticType {
+    DEATH,
+    KILL,
+    PLAYTIME,
+    REVIVE,
+    SERVICE
+}
--- src/main/java/com/rettichlp/UnicacityAddon/events/faction/ServiceCountEventHandler.java
+++ src/main/java/com/rettichlp/UnicacityAddon/events/faction/ServiceCountEventHandler.java
@@ -1,5 +1,6 @@
 package com.rettichlp.UnicacityAddon.events.faction;
 
+import com.rettichlp.UnicacityAddon.base.api.enums.StatisticType;
 import com.rettichlp.UnicacityAddon.base.api.request.APIRequest;
 import com.rettichlp.UnicacityAddon.base.registry.annotation.UCEvent;
 import com.rettichlp.UnicacityAddon.base.text.PatternHandler;
@@ -17,7 +18,7 @@ public class ServiceCountEventHandler {
     public boolean onClientChatReceived(ClientChatReceivedEvent e) {
         if (PatternHandler.SERVICE_DONE_PATTERN.matcher(e.getMessage().getUnformattedText()).find()) {
             ServiceCountCommand.addService();
-            APIRequest.sendStatisticAddRequest("SERVICE");
+            APIRequest.sendStatisticAddRequest(StatisticType.SERVICE);
         }
 
         return false;
