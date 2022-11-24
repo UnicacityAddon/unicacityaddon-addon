f1a44988ef40d3d386571672d04034ef9201731e
rettichlp <rettichlp01@gmail.com>
2022-11-24 18:19:56 +0100

    Migrate jobs to one JobEventHandler

---
+++ src/main/java/com/rettichlp/UnicacityAddon/base/api/enums/DropPosition.java
@@ -0,0 +1,30 @@
+package com.rettichlp.UnicacityAddon.base.api.enums;
+
+public enum DropPosition {
+    GLAS(885, 67, 350),
+    WASTE(906, 66, 361),
+    METAL(899, 67, 394),
+    WOOD(874, 68, 378);
+
+    final int x;
+    final int y;
+    final int z;
+
+    DropPosition(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+}
