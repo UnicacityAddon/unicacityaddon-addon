9adde2d1212eaeae063017edd4e267b546be018a
rettichlp <rettichlp01@gmail.com>
2022-12-06 13:49:40 +0100

    Add getDrugTypeByNameOrShortName to DrugType

--- src/main/java/com/rettichlp/unicacityaddon/base/enums/faction/badfaction/DrugType.java
+++ src/main/java/com/rettichlp/unicacityaddon/base/enums/faction/badfaction/DrugType.java
@@ -1,5 +1,7 @@
 package com.rettichlp.unicacityaddon.base.enums.faction.badfaction;
 
+import java.util.Arrays;
+
 public enum DrugType {
 
     COCAINE("Kokain", "Koks"), MARIJUANA("Marihuana", "Gras"), METH("Methamphetamin", "Meth"), LSD("LSD", "LSD");
@@ -18,5 +20,11 @@ public enum DrugType {
 
     public String getShortName() {
         return shortName;
+    }
+
+    public static DrugType getDrugTypeByNameOrShortName(String drugName) {
+        return Arrays.stream(DrugType.values())
+                .filter(drugType -> drugType.drugName.equalsIgnoreCase(drugName) || drugType.shortName.equalsIgnoreCase(drugName))
+                .findFirst().orElseThrow(() -> new RuntimeException("DrugType does not exist"));
     }
 }
