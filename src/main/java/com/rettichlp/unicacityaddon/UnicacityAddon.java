ae882462049ad75fceb6240f28ef66571896e4e6
rettichlp <rettichlp01@gmail.com>
2023-01-30 12:37:18 +0100

    Update version to 1.9.0-dev

--- src/main/java/com/rettichlp/unicacityaddon/UnicacityAddon.java
+++ src/main/java/com/rettichlp/unicacityaddon/UnicacityAddon.java
@@ -34,7 +34,7 @@ import java.util.List;
 @Mod(name = "UnicacityAddon", modid = "unicacityaddon", version = UnicacityAddon.VERSION, clientSideOnly = true, acceptedMinecraftVersions = "[1.12,1.12.2]")
 public class UnicacityAddon extends LabyModAddon {
 
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.9.0-dev";
     public static final Minecraft MINECRAFT = Minecraft.getMinecraft();
     public static UnicacityAddon ADDON;
     public static final Logger LOGGER = LogManager.getLogger();
