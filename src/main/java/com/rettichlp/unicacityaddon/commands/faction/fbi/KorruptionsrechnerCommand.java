da10ae7f640ff8f9e737a136b52658681d951556
Gelegenheitscode <leonard-nolde@web.de>
2023-02-14 19:00:12 +0100

    #607 #609 #608

---
+++ src/main/java/com/rettichlp/unicacityaddon/commands/faction/fbi/KorruptionsrechnerCommand.java
@@ -0,0 +1,126 @@
+package com.rettichlp.unicacityaddon.commands.faction.fbi;
+
+import com.rettichlp.unicacityaddon.base.abstraction.AbstractionLayer;
+import com.rettichlp.unicacityaddon.base.abstraction.UPlayer;
+import com.rettichlp.unicacityaddon.base.builder.TabCompletionBuilder;
+import com.rettichlp.unicacityaddon.base.registry.annotation.UCCommand;
+import com.rettichlp.unicacityaddon.base.text.ColorCode;
+import com.rettichlp.unicacityaddon.base.text.Message;
+import com.rettichlp.unicacityaddon.base.utils.ForgeUtils;
+import com.rettichlp.unicacityaddon.events.faction.polizei.WantedEventHandler;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.client.IClientCommand;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Gelegenheitscode
+ */
+
+@UCCommand
+public class KorruptionsrechnerCommand implements IClientCommand {
+
+    @Override
+    public boolean allowUsageWithoutPrefix(ICommandSender sender, String message) {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return "korruptionsrechner";
+    }
+
+    @Override
+    public String getUsage(@Nonnull ICommandSender sender) {
+        return "/korruptionsrechner [Spieler]";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) {
+        UPlayer p = AbstractionLayer.getPlayer();
+        if (args.length < 1) {
+            p.sendSyntaxMessage(getUsage(sender));
+            return;
+        }
+        String target = args[0];
+
+        WantedEventHandler.Wanted wanted = WantedEventHandler.WANTED_MAP.get(target);
+        if (wanted == null) {
+            p.sendErrorMessage("Du hast /wanteds noch nicht ausgeführt!");
+            return;
+        }
+        int Betrag;
+        int Drugs;
+        int MethDrugs0;
+        int MethDrugs1;
+        int MethDrugs2;
+        Betrag = wanted.getAmount() * 150;
+        Drugs = Betrag / 55;
+        MethDrugs0 = Betrag / 110;
+        MethDrugs1 = Betrag / 100;
+        MethDrugs2 = Betrag / 50;
+        p.sendMessage(Message.getBuilder()
+                .of("»").color(ColorCode.GRAY).advance().space()
+                .of("Korruptionspreise für").color(ColorCode.DARK_AQUA).advance().space()
+                .of(target).color(ColorCode.DARK_AQUA).advance()
+                .of(":").color(ColorCode.DARK_GRAY).advance().space()
+                .createComponent());
+        p.sendMessage(Message.getBuilder()
+                .of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                .of("Geld").color(ColorCode.DARK_AQUA).advance()
+                .of(":").color(ColorCode.DARK_GRAY).advance().space()
+                .of(Betrag + "$").color(ColorCode.AQUA).advance()
+                .createComponent());
+        p.sendMessage(Message.getBuilder()
+                .of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                .of("Drogen").color(ColorCode.DARK_AQUA).advance()
+                .of(":").color(ColorCode.DARK_GRAY).advance().space()
+                .of(Drugs + "g").color(ColorCode.AQUA).advance()
+                .createComponent());
+        p.sendMessage(Message.getBuilder()
+                .of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                .of("Meth").color(ColorCode.DARK_AQUA).advance()
+                .of(":").color(ColorCode.DARK_GRAY).advance().space()
+                .of(MethDrugs0 + "g").color(ColorCode.AQUA).advance().space()
+                .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                .of(MethDrugs1 + "g").color(ColorCode.AQUA).advance().space()
+                .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                .of(MethDrugs2 + "g").color(ColorCode.AQUA).advance()
+                .createComponent());
+
+    }
+
+    @Override
+    public boolean checkPermission(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    @Nonnull
+    public List<String> getTabCompletions(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args, @Nullable BlockPos targetPos) {
+        return TabCompletionBuilder.getBuilder(args)
+                .addToAllFromIndex(2, ForgeUtils.getOnlinePlayers())
+                .build();
+    }
+
+    @Override
+    public boolean isUsernameIndex(@Nonnull String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(@Nonnull ICommand o) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/rettichlp/unicacityaddon/events/faction/polizei/WantedListEventHandler.java
@@ -0,0 +1,164 @@
+package com.rettichlp.unicacityaddon.events.faction.polizei;
+
+import com.rettichlp.unicacityaddon.base.config.ConfigElements;
+import com.rettichlp.unicacityaddon.base.registry.annotation.UCEvent;
+import com.rettichlp.unicacityaddon.base.text.ColorCode;
+import com.rettichlp.unicacityaddon.base.text.Message;
+import com.rettichlp.unicacityaddon.base.text.PatternHandler;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.regex.Matcher;
+
+/**
+ * @author Gelegenheitscode
+ */
+
+@UCEvent
+public class WantedListEventHandler {
+    @SubscribeEvent
+    public void onChat(ClientChatReceivedEvent e)   {
+        if (!ConfigElements.getBetterWantedList())  {
+            return;
+        }
+        String msg = e.getMessage().getUnformattedText();
+
+        Matcher m = PatternHandler.WANTED_LIST_PATTERN.matcher(msg);
+        if (m.find())   {
+            int WantedAmount = Integer.parseInt(m.group(2));
+            if (WantedAmount == 1) {
+                if (msg.contains("AFK")) {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.DARK_GREEN).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.DARK_GREEN).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of("AFK").color(ColorCode.GRAY).advance()
+                            .createComponent());
+                } else  {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.DARK_GREEN).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.DARK_GREEN).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .createComponent());
+                }
+            } else if (WantedAmount <= 15 && WantedAmount >= 2) {
+                if (msg.contains("AFK")) {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.GREEN).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.GREEN).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of("AFK").color(ColorCode.GRAY).advance()
+                            .createComponent());
+                } else  {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.GREEN).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.GREEN).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .createComponent());
+                }
+            } else if (WantedAmount <= 25 && WantedAmount >= 16)    {
+                if (msg.contains("AFK")) {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.YELLOW).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.YELLOW).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of("AFK").color(ColorCode.GRAY).advance()
+                            .createComponent());
+                } else  {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.YELLOW).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.YELLOW).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .createComponent());
+                }
+            } else if (WantedAmount <= 49 && WantedAmount >26)  {
+                if (msg.contains("AFK")) {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.GOLD).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.GOLD).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of("AFK").color(ColorCode.GRAY).advance()
+                            .createComponent());
+                } else  {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.GOLD).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.GOLD).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .createComponent());
+                }
+            } else if (WantedAmount <= 59 && WantedAmount >=50) {
+                if (msg.contains("AFK")) {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.RED).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.RED).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of("AFK").color(ColorCode.GRAY).advance()
+                            .createComponent());
+                } else  {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.RED).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.RED).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .createComponent());
+                }
+            } else if (WantedAmount <= 69 && WantedAmount >= 60)    {
+                if (msg.contains("AFK")) {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.DARK_RED).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.DARK_RED).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of("AFK").color(ColorCode.GRAY).advance()
+                            .createComponent());
+                } else  {
+                    e.setMessage(Message.getBuilder().of("▶").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(1)).color(ColorCode.DARK_RED).advance().space()
+                            .of("|").color(ColorCode.DARK_GRAY).advance().space()
+                            .of(m.group(3)).color(ColorCode.DARK_RED).advance().space()
+                            .of("(").color(ColorCode.GRAY).advance()
+                            .of(m.group(2)).color(ColorCode.BLUE).advance()
+                            .of(")").color(ColorCode.GRAY).advance().space()
+                            .createComponent());
+                }
+            }
+        }
+    }
+}
