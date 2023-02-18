029c73c926bd223668125797d08a61b903bb1a91
Gelegenheitscode <90222460+Gelegenheitscode@users.noreply.github.com>
2023-02-19 00:26:13 +0100

    Update src/main/java/com/rettichlp/unicacityaddon/commands/faction/fbi/KorruptionsrechnerCommand.java
    Co-authored-by: RettichLP <97811064+rettichlp@users.noreply.github.com>

--- src/main/java/com/rettichlp/unicacityaddon/commands/faction/fbi/KorruptionsrechnerCommand.java
+++ src/main/java/com/rettichlp/unicacityaddon/commands/faction/fbi/KorruptionsrechnerCommand.java
@@ -65,7 +65,7 @@ public class KorruptionsrechnerCommand implements IClientCommand {
         int drugs = money / 55;
         int methDrugs0 = money / 110;
         int methDrugs1 = money / 100;
-        MethDrugs2 = Betrag / 50;
+        int methDrugs2 = money / 50;
         p.sendMessage(Message.getBuilder()
                 .of("Korruptionspreise für").color(ColorCode.DARK_AQUA).advance().space()
                 .of(target).color(ColorCode.DARK_AQUA).advance()
