50cda6454aa28e0eb167043229e58965b9179c70
Gelegenheitscode <leonard-nolde@web.de>
2023-02-24 22:45:58 +0100

    implement clear command

---
+++ src/main/java/com/rettichlp/unicacityaddon/commands/faction/polizei/ClearCommand.java
@@ -0,0 +1,91 @@
+package com.rettichlp.unicacityaddon.commands.faction.polizei;
+
+import com.rettichlp.unicacityaddon.base.abstraction.AbstractionLayer;
+import com.rettichlp.unicacityaddon.base.abstraction.UPlayer;
+import com.rettichlp.unicacityaddon.base.builder.TabCompletionBuilder;
+import com.rettichlp.unicacityaddon.base.registry.annotation.UCCommand;
+import com.rettichlp.unicacityaddon.base.utils.ForgeUtils;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.client.IClientCommand;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Gelegenheitscode
+ */
+@UCCommand
+public class ClearCommand implements IClientCommand {
+
+    @Override
+    @Nonnull
+    public String getName() {
+        return "clear";
+    }
+
+    @Override
+    @Nonnull
+    public String getUsage(@Nonnull ICommandSender sender) {
+        return "/clear [Spieler...]";
+    }
+
+    @Override
+    @Nonnull
+    public List<String> getAliases() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean checkPermission(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, String[] args) {
+        UPlayer p = AbstractionLayer.getPlayer();
+        if (args.length < 1) {
+            p.sendSyntaxMessage(getUsage(sender));
+            return;
+        }
+
+        List<String> players = Arrays.asList(args);
+
+        clearWanteds(p, players);
+    }
+
+    private void clearWanteds(UPlayer issuer, List<String> players) {
+        for (String player : players) {
+            issuer.sendChatMessage("/clear " + player);
+        }
+    }
+
+    @Override
+    @Nonnull
+    public List<String> getTabCompletions(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args, @Nullable BlockPos targetPos) {
+        return TabCompletionBuilder.getBuilder(args)
+                .addToAllFromIndex(2, ForgeUtils.getOnlinePlayers())
+                .build();
+    }
+
+    @Override
+    public boolean isUsernameIndex(@Nonnull String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public boolean allowUsageWithoutPrefix(ICommandSender sender, String message) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(@Nonnull ICommand o) {
+        return 0;
+    }
+
+}
