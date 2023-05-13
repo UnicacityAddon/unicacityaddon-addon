93ad1d04c10f455fa20b1cf5359222ce590c682b
rettichlp <rettichlp01@gmail.com>
2023-05-13 23:05:43 +0200

    Update licenses

--- src/main/java/com/rettichlp/unicacityaddon/base/teamspeak/commands/ChannelListCommand.java
+++ src/main/java/com/rettichlp/unicacityaddon/base/teamspeak/commands/ChannelListCommand.java
@@ -9,6 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * The following code is based on MPL-licensed code by Paul Zhang.
+ * The original code is available at: <a href="https://github.com/paulzhng/UCUtils">https://github.com/paulzhng/UCUtils</a>.
+ * Copyright (c) 2019/2020 Paul Zhang
+ * <p>
+ * The following code is subject to the MPL Version 2.0.
+ *
  * @author Fuzzlemann
  */
 public class ChannelListCommand extends BaseCommand<ChannelListCommand.Response> {
--- src/main/java/com/rettichlp/unicacityaddon/base/teamspeak/events/ClientMessageReceivedEvent.java
+++ src/main/java/com/rettichlp/unicacityaddon/base/teamspeak/events/ClientMessageReceivedEvent.java
@@ -4,6 +4,12 @@ import com.rettichlp.unicacityaddon.base.teamspeak.CommandResponse;
 import com.rettichlp.unicacityaddon.base.teamspeak.objects.TargetMode;
 
 /**
+ * The following code is based on MPL-licensed code by Paul Zhang.
+ * The original code is available at: <a href="https://github.com/paulzhng/UCUtils">https://github.com/paulzhng/UCUtils</a>.
+ * Copyright (c) 2019/2020 Paul Zhang
+ * <p>
+ * The following code is subject to the MPL Version 2.0.
+ *
  * @author Fuzzlemann
  */
 @TSEvent.Name("notifytextmessage")
