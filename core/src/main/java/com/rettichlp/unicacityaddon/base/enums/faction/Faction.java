1cfa55126315d40747222583f8fd1bbe027730a2
rettichlp <rettichlp01@gmail.com>
2023-06-05 12:07:31 +0200

    Add lombok and bad faction field to Faction

--- core/src/main/java/com/rettichlp/unicacityaddon/base/enums/faction/Faction.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/base/enums/faction/Faction.java
@@ -2,64 +2,68 @@ package com.rettichlp.unicacityaddon.base.enums.faction;
 
 import com.rettichlp.unicacityaddon.base.text.ColorCode;
 import com.rettichlp.unicacityaddon.base.text.Message;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * @author RettichLP
  */
+@Getter
+@AllArgsConstructor
 public enum Faction {
 
-    NULL("", "Keine Auswahl", "", -1, ""),
-    FBI("fbi", "FBI", "FBI", 106, Message.getBuilder()
+    NULL("", "Keine Auswahl", "", false, -1, ""),
+    FBI("fbi", "FBI", "FBI", false, 106, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✯").color(ColorCode.DARK_BLUE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    POLIZEI("polizei", "Polizei", "Polizei", 78, Message.getBuilder()
+    POLIZEI("polizei", "Polizei", "Polizei", false, 78, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✯").color(ColorCode.BLUE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    RETTUNGSDIENST("rettungsdienst", "Rettungsdienst", "Rettungsdienst", 118, Message.getBuilder()
+    RETTUNGSDIENST("rettungsdienst", "Rettungsdienst", "Rettungsdienst", false, 118, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✚").color(ColorCode.DARK_RED).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
 
-    CALDERON("calderon", "Calderón Kartell", "Kartell", 154, Message.getBuilder()
+    CALDERON("calderon", "Calderón Kartell", "Kartell", true, 154, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("☀").color(ColorCode.GOLD).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    KERZAKOV("kerzakov", "Kerzakov Familie", "Kerzakov", 166, Message.getBuilder()
+    KERZAKOV("kerzakov", "Kerzakov Familie", "Kerzakov", true, 166, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✮").color(ColorCode.RED).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    LACOSANOSTRA("lacosanostra", "La Cosa Nostra", "Mafia", 130, Message.getBuilder()
+    LACOSANOSTRA("lacosanostra", "La Cosa Nostra", "Mafia", true, 130, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("⚜").color(ColorCode.DARK_AQUA).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    LEMILIEU("le_milieu", "Le Milieu", "France", 179, Message.getBuilder()
+    LEMILIEU("le_milieu", "Le Milieu", "France", true, 179, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("Ⓜ").color(ColorCode.DARK_AQUA).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    OBRIEN("obrien", "O'brien Familie", "Obrien", 191, Message.getBuilder()
+    OBRIEN("obrien", "O'brien Familie", "Obrien", true, 191, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("☘").color(ColorCode.DARK_GREEN).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    WESTSIDEBALLAS("westsideballas", "Westside Ballas", "Gang", 142, Message.getBuilder()
+    WESTSIDEBALLAS("westsideballas", "Westside Ballas", "Gang", true, 142, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("☠").color(ColorCode.DARK_PURPLE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
 
-    HITMAN("hitman", "Hitman", "Hitman", 215, Message.getBuilder()
+    HITMAN("hitman", "Hitman", "Hitman", false, 215, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("➹").color(ColorCode.AQUA).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    KIRCHE("kirche", "Kirche", "Kirche", 227, Message.getBuilder()
+    KIRCHE("kirche", "Kirche", "Kirche", false, 227, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("†").color(ColorCode.LIGHT_PURPLE).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    NEWS("news", "News Agency", "News", 239, Message.getBuilder()
+    NEWS("news", "News Agency", "News", false, 239, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("✉").color(ColorCode.YELLOW).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    TERRORISTEN("terroristen", "Terroristen", "Terroristen", 203, Message.getBuilder()
+    TERRORISTEN("terroristen", "Terroristen", "Terroristen", false, 203, Message.getBuilder()
             .of("◤").color(ColorCode.DARK_GRAY).advance()
             .of("❇").color(ColorCode.GRAY).advance()
             .of("◢").color(ColorCode.DARK_GRAY).advance().create());
@@ -67,36 +71,9 @@ public enum Faction {
     private final String apiName;
     private final String displayName;
     private final String factionKey;
+    private final boolean isBadFaction;
     private final int publicChannelId;
     private final String nameTagSuffix;
-
-    Faction(String apiName, String displayName, String factionKey, int publicChannelId, String nameTagSuffix) {
-        this.apiName = apiName;
-        this.displayName = displayName;
-        this.factionKey = factionKey;
-        this.publicChannelId = publicChannelId;
-        this.nameTagSuffix = nameTagSuffix;
-    }
-
-    public String getApiName() {
-        return this.apiName;
-    }
-
-    public String getDisplayName() {
-        return this.displayName;
-    }
-
-    public String getFactionKey() {
-        return this.factionKey;
-    }
-
-    public int getPublicChannelId() {
-        return publicChannelId;
-    }
-
-    public String getNameTagSuffix() {
-        return nameTagSuffix;
-    }
 
     public String getWebsiteUrl() {
         return this.equals(NULL) ? null : "https://unicacity.de/fraktionen/" + getApiName();
