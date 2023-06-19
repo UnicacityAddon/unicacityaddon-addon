43c5b6544256adf57717c09ba901d8e55071e270
rettichlp <rettichlp01@gmail.com>
2023-06-19 22:35:48 +0200

    Add number format to money hud widgets

--- core/src/main/java/com/rettichlp/unicacityaddon/hudwidgets/JobHudWidget.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/hudwidgets/JobHudWidget.java
@@ -7,11 +7,15 @@ import net.labymod.api.client.gui.hud.hudwidget.text.TextHudWidgetConfig;
 import net.labymod.api.client.gui.hud.hudwidget.text.TextLine;
 import net.labymod.api.event.Subscribe;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  * @author RettichLP
  */
 public class JobHudWidget extends TextHudWidget<TextHudWidgetConfig> {
 
+    private final NumberFormat numberFormat = NumberFormat.getNumberInstance(new Locale("da", "DK"));
     private TextLine textLine;
 
     private final UnicacityAddon unicacityAddon;
@@ -24,12 +28,12 @@ public class JobHudWidget extends TextHudWidget<TextHudWidgetConfig> {
     @Override
     public void load(TextHudWidgetConfig config) {
         super.load(config);
-        this.textLine = super.createLine("Job", this.unicacityAddon.services().file().data().getJobBalance() + "$ | " + this.unicacityAddon.services().file().data().getJobExperience() + " EXP");
+        this.textLine = super.createLine("Job", this.numberFormat.format(this.unicacityAddon.services().file().data().getJobBalance()) + "$ | " + this.unicacityAddon.services().file().data().getJobExperience() + " EXP");
         this.setIcon(this.unicacityAddon.services().util().icon());
     }
 
     @Subscribe
     public void onOfflineDataChanged(OfflineDataChangedEvent e) {
-        this.textLine.updateAndFlush(e.getData().getJobBalance() + "$ | " + e.getData().getJobExperience() + " EXP");
+        this.textLine.updateAndFlush(this.numberFormat.format(e.getData().getJobBalance()) + "$ | " + e.getData().getJobExperience() + " EXP");
     }
 }
