5f1d4e45f7194f7c2aa71c2f4c7a11d80be294c8
Dimiikou <leon.aissa@icloud.com>
2023-07-24 00:58:14 +0200

    Add ProtectionMoneyCommand

--- core/src/main/java/com/rettichlp/unicacityaddon/base/enums/Activity.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/base/enums/Activity.java
@@ -2,5 +2,5 @@ package com.rettichlp.unicacityaddon.base.enums;
 
 public enum Activity {
 
-    MONEY, DRUG, ROLEPLAY, EQUIP_ADD, EQUIP_EDIT
+    MONEY, DRUG, ROLEPLAY, EQUIP_ADD, EQUIP_EDIT, PROTECTION_MONEY
 }
