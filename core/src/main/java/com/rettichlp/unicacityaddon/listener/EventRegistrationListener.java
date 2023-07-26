df2a1aee927ca437ede408872bff34d343155934
rettichlp <rettichlp01@gmail.com>
2023-07-26 03:27:21 +0200

    Update nearest gas station message in CarListener and code cleanup in EventRegistrationListener

--- core/src/main/java/com/rettichlp/unicacityaddon/listener/EventRegistrationListener.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/listener/EventRegistrationListener.java
@@ -74,15 +74,15 @@ public class EventRegistrationListener {
         Key key = e.key();
 
         UnicacityAddonConfiguration configuration = this.unicacityAddon.configuration();
-        if (state.equals(KeyEvent.State.PRESS)) {
-            if (key.equals(Key.TAB) && !Laby.references().chatAccessor().isChatOpen() && configuration.tablist().enabled().get() && configuration.tablist().sorted().get() && this.unicacityAddon.utilService().isUnicacity()) {
+        if (state.equals(KeyEvent.State.PRESS) && this.unicacityAddon.utilService().isUnicacity()) {
+            if (key.equals(Key.TAB) && !Laby.references().chatAccessor().isChatOpen() && configuration.tablist().enabled().get() && configuration.tablist().sorted().get()) {
                 this.unicacityAddon.utilService().debug("Sorting tab list");
                 this.unicacityAddon.tabListController().orderTabList(this.unicacityAddon);
                 return;
             }
 
             HotkeyConfiguration hotkeyConfiguration = configuration.hotkey();
-            if (!Laby.references().chatAccessor().isChatOpen() && this.unicacityAddon.utilService().isUnicacity() && hotkeyConfiguration.enabled().get()) {
+            if (!Laby.references().chatAccessor().isChatOpen() && hotkeyConfiguration.enabled().get()) {
                 Laby.labyAPI().eventBus().fire(new HotkeyEvent(this.unicacityAddon, key));
             }
         }
