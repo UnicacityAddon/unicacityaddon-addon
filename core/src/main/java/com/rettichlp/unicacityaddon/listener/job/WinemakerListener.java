c080b34753d301fef463127af71cc3a5ec3efea2
rettichlp <rettichlp01@gmail.com>
2023-07-29 18:48:18 +0200

    Adjust mouse action

--- core/src/main/java/com/rettichlp/unicacityaddon/listener/job/WinemakerListener.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/listener/job/WinemakerListener.java
@@ -3,15 +3,14 @@ package com.rettichlp.unicacityaddon.listener.job;
 import com.rettichlp.unicacityaddon.UnicacityAddon;
 import com.rettichlp.unicacityaddon.base.registry.annotation.UCEvent;
 import com.rettichlp.unicacityaddon.base.text.ColorCode;
+import com.rettichlp.unicacityaddon.base.text.PatternHandler;
+import net.labymod.api.Laby;
+import net.labymod.api.client.gui.mouse.Mouse;
 import net.labymod.api.client.world.item.ItemStack;
 import net.labymod.api.event.Subscribe;
-import net.labymod.api.event.client.gui.screen.ScreenOpenEvent;
+import net.labymod.api.event.client.chat.ChatReceiveEvent;
+import net.labymod.api.event.client.render.ScreenRenderEvent;
 import net.labymod.api.event.client.world.ItemStackTooltipEvent;
-
-import java.awt.AWTException;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.Robot;
 
 /**
  * @author RettichLP
@@ -19,7 +18,9 @@ import java.awt.Robot;
 @UCEvent
 public class WinemakerListener {
 
-    private Point lastMousePos;
+    private int containerId;
+    private Double lastMousePosX;
+    private Double lastMousePosY;
 
     private final UnicacityAddon unicacityAddon;
 
@@ -28,28 +29,37 @@ public class WinemakerListener {
     }
 
     @Subscribe
-    public void onItemStackTooltip(ItemStackTooltipEvent e) {
-        ItemStack itemStack = e.itemStack();
-        String plainDisplayName = this.unicacityAddon.utilService().text().plain(itemStack.getDisplayName());
+    public void onChatReceive(ChatReceiveEvent e) {
+        String msg = e.chatMessage().getPlainText();
 
-        if (plainDisplayName.equals("Traube")) {
-            this.lastMousePos = MouseInfo.getPointerInfo().getLocation();
+        if (PatternHandler.WINEMAKER_CONTINUE_PATTERN.matcher(msg).find()) {
+            this.lastMousePosX = null;
+            this.lastMousePosY = null;
         }
     }
 
     @Subscribe
-    public void onScreenOpen(ScreenOpenEvent e) {
+    public void onItemStackTooltip(ItemStackTooltipEvent e) {
+        ItemStack itemStack = e.itemStack();
+        String plainDisplayName = this.unicacityAddon.utilService().text().plain(itemStack.getDisplayName());
+
+        this.containerId = this.unicacityAddon.guiController().getContainerId();
+
+        if (plainDisplayName.equals("Traube")) {
+            Mouse mouse = Laby.labyAPI().minecraft().mouse();
+            this.lastMousePosX = mouse.getXDouble();
+            this.lastMousePosY = mouse.getYDouble();
+        }
+    }
+
+    @Subscribe
+    public void onScreenRender(ScreenRenderEvent e) {
         String containerLegacyName = this.unicacityAddon.guiController().getContainerLegacyName();
         boolean isWinemakerContainer = containerLegacyName != null && containerLegacyName.equals(ColorCode.GOLD.getCode() + "Winzer");
 
-        if (isWinemakerContainer && this.lastMousePos != null) {
-            try {
-                Robot robot = new Robot();
-                robot.mouseMove(this.lastMousePos.x, this.lastMousePos.y);
-                this.lastMousePos = null;
-            } catch (AWTException ex) {
-                this.unicacityAddon.logger().warn(ex.getMessage());
-            }
+        int currentContainerId = this.unicacityAddon.guiController().getContainerId();
+        if (isWinemakerContainer && this.containerId != currentContainerId && this.lastMousePosX != null && this.lastMousePosY != null) {
+            Laby.labyAPI().minecraft().setCursorPosition(this.lastMousePosX, -this.lastMousePosY);
         }
     }
 }
