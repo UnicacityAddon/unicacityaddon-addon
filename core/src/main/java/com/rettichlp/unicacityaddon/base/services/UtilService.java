f84aa1a2e9aafc57fa64485909a5c867ffa5a198
rettichlp <rettichlp01@gmail.com>
2023-08-07 21:03:25 +0200

    Update version to 2.3.0-dev

--- core/src/main/java/com/rettichlp/unicacityaddon/base/services/UtilService.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/base/services/UtilService.java
@@ -52,7 +52,7 @@ public class UtilService {
 
     @SuppressWarnings("SameReturnValue")
     public String version() {
-        return "2.2.0";
+        return "2.3.0-dev";
     }
 
     public boolean isUnicacity() {
