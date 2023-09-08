49c6048365cfd263ff9defed83b9cdc5a05fda1f
rettichlp <rettichlp01@gmail.com>
2023-09-08 13:49:58 +0200

    Add color to faction

--- core/src/main/java/com/rettichlp/unicacityaddon/base/enums/faction/Faction.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/base/enums/faction/Faction.java
@@ -12,61 +12,22 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Faction {
 
-    NULL("", "Keine Auswahl", "", false, -1, -1, ""),
-    FBI("fbi", "FBI", "FBI", false, 106, 107, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("✯").color(ColorCode.DARK_BLUE).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    POLIZEI("polizei", "Polizei", "Polizei", false, 78, 79, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("✯").color(ColorCode.BLUE).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    RETTUNGSDIENST("rettungsdienst", "Rettungsdienst", "Rettungsdienst", false, 118, 119, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("✚").color(ColorCode.DARK_RED).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
+    NULL("", "Keine Auswahl", "", false, -1, -1, ColorCode.GRAY, ""),
+    FBI("fbi", "FBI", "FBI", false, 106, 107, ColorCode.DARK_BLUE, "✯"),
+    POLIZEI("polizei", "Polizei", "Polizei", false, 78, 79, ColorCode.BLUE, "✯"),
+    RETTUNGSDIENST("rettungsdienst", "Rettungsdienst", "Rettungsdienst", false, 118, 119, ColorCode.DARK_RED, "✚"),
 
-    CALDERON("calderon", "Calderón Kartell", "Kartell", true, 154, 155, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("☀").color(ColorCode.GOLD).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    KERZAKOV("kerzakov", "Kerzakov Familie", "Kerzakov", true, 166, 167, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("✮").color(ColorCode.RED).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    LACOSANOSTRA("lacosanostra", "La Cosa Nostra", "Mafia", true, 130, 131, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("⚜").color(ColorCode.DARK_AQUA).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    LEMILIEU("le_milieu", "Le Milieu", "France", true, 179, 180, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("Ⓜ").color(ColorCode.DARK_AQUA).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    OBRIEN("obrien", "O'brien Familie", "Obrien", true, 191, 192, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("☘").color(ColorCode.DARK_GREEN).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    WESTSIDEBALLAS("westsideballas", "Westside Ballas", "Gang", true, 142, 143, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("☠").color(ColorCode.DARK_PURPLE).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
+    CALDERON("calderon", "Calderón Kartell", "Kartell", true, 154, 155, ColorCode.GOLD, "☀"),
+    KERZAKOV("kerzakov", "Kerzakov Familie", "Kerzakov", true, 166, 167, ColorCode.RED, "✮"),
+    LACOSANOSTRA("lacosanostra", "La Cosa Nostra", "Mafia", true, 130, 131, ColorCode.DARK_AQUA, "⚜"),
+    LEMILIEU("le_milieu", "Le Milieu", "France", true, 179, 180, ColorCode.DARK_AQUA, "Ⓜ"),
+    OBRIEN("obrien", "O'brien Familie", "Obrien", true, 191, 192, ColorCode.DARK_GREEN, "☘"),
+    WESTSIDEBALLAS("westsideballas", "Westside Ballas", "Gang", true, 142, 143, ColorCode.DARK_PURPLE, "☠"),
 
-    HITMAN("hitman", "Hitman", "Hitman", false, 215, 216, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("➹").color(ColorCode.AQUA).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    KIRCHE("kirche", "Kirche", "Kirche", false, 227, 228, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("†").color(ColorCode.LIGHT_PURPLE).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    NEWS("news", "News Agency", "News", false, 239, 240, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("✉").color(ColorCode.YELLOW).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create()),
-    TERRORISTEN("terroristen", "Terroristen", "Terroristen", false, 203, 204, Message.getBuilder()
-            .of("◤").color(ColorCode.DARK_GRAY).advance()
-            .of("❇").color(ColorCode.GRAY).advance()
-            .of("◢").color(ColorCode.DARK_GRAY).advance().create());
+    HITMAN("hitman", "Hitman", "Hitman", false, 215, 216, ColorCode.AQUA, "➹"),
+    KIRCHE("kirche", "Kirche", "Kirche", false, 227, 228, ColorCode.LIGHT_PURPLE, "†"),
+    NEWS("news", "News Agency", "News", false, 239, 240, ColorCode.YELLOW, "✉"),
+    TERRORISTEN("terroristen", "Terroristen", "Terroristen", false, 203, 204, ColorCode.GRAY, "❇");
 
     private final String apiName;
     private final String displayName;
@@ -74,10 +35,18 @@ public enum Faction {
     private final boolean isBadFaction;
     private final int publicChannelId;
     private final int abstractedChannelId;
-    private final String nameTagSuffix;
+    private final ColorCode color;
+    private final String icon;
 
     public String getWebsiteUrl() {
         return this.equals(NULL) ? null : "https://unicacity.de/fraktionen/" + getApiName();
+    }
+
+    public String getNameTagSuffix() {
+        return Message.getBuilder()
+                .of("◤").color(ColorCode.DARK_GRAY).advance()
+                .of(this.icon).color(this.color).advance()
+                .of("◢").color(ColorCode.DARK_GRAY).advance().create();
     }
 
     public static Faction getFactionByDisplayName(String s) {
