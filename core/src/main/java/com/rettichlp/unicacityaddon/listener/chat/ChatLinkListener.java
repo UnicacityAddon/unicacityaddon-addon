5a02ed241b1ac290ac39404fc8bfce727cea12ee
rettichlp <rettichlp01@gmail.com>
2023-09-09 11:00:44 +0200

    Add create title method to TextUtils

--- core/src/main/java/com/rettichlp/unicacityaddon/listener/chat/ChatLinkListener.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/listener/chat/ChatLinkListener.java
@@ -80,21 +80,4 @@ public class ChatLinkListener {
 
         e.setMessage(mainComponent);
     }
-
-    private String createTitle(String title) {
-        StringBuilder titleBuilder = new StringBuilder();
-        boolean nextTitleCase = true;
-
-        for (char c : title.replace("-", " ").replace("_", " ").toCharArray()) {
-            if (Character.isSpaceChar(c)) {
-                nextTitleCase = true;
-            } else if (nextTitleCase) {
-                c = Character.toTitleCase(c);
-                nextTitleCase = false;
-            }
-            titleBuilder.append(c);
-        }
-
-        return titleBuilder.toString();
-    }
 }
