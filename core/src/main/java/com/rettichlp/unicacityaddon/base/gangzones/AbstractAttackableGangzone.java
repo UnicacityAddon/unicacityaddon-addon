dc73793dd4306ecc8ab3d23ce35c80d4234ff576
rettichlp <rettichlp01@gmail.com>
2023-09-10 22:37:12 +0200

    Remove UCGangzone annotation from AbstractAttackableGangzone

--- core/src/main/java/com/rettichlp/unicacityaddon/base/gangzones/AbstractAttackableGangzone.java
+++ core/src/main/java/com/rettichlp/unicacityaddon/base/gangzones/AbstractAttackableGangzone.java
@@ -2,6 +2,7 @@ package com.rettichlp.unicacityaddon.base.gangzones;
 
 import com.rettichlp.unicacityaddon.UnicacityAddon;
 import com.rettichlp.unicacityaddon.base.registry.annotation.UCGangzone;
+import com.rettichlp.unicacityaddon.base.text.ColorCode;
 import net.labymod.api.util.Color;
 import net.labymod.api.util.Pair;
 import net.labymod.api.util.math.vector.FloatVector3;
@@ -10,20 +11,17 @@ import java.util.List;
 
 public abstract class AbstractAttackableGangzone extends AbstractGangzone {
 
-    private final UCGangzone ucGangzone;
-
     private final UnicacityAddon unicacityAddon;
 
     public AbstractAttackableGangzone(UnicacityAddon unicacityAddon, UCGangzone ucGangzone) {
         super(unicacityAddon, ucGangzone);
         this.unicacityAddon = unicacityAddon;
-        this.ucGangzone = ucGangzone;
     }
 
     public abstract List<Pair<FloatVector3, FloatVector3>> gangwarFacades();
 
     public void renderGangwarArea() {
-        Color color = ucGangzone.owner().getColor().getLabymodColor();
+        Color color = ColorCode.GOLD.getLabymodColor();
         this.gangwarFacades().forEach(posPair -> this.unicacityAddon.renderController().drawFacade(posPair.getFirst(), posPair.getSecond(), color));
     }
 }
